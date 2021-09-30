0c6f1f9f97d41f4da55fecef40c9eed99ec060e0
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-09-30 15:17:10 -0700

    added react router with landing page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Quotes from './Quotes.js';
 import Character from './Character.js';
 
@@ -6,10 +12,22 @@ export default class App extends Component {
   render() {
     return (
       <div className="Main">
-        <h1>Welcome to the Universe</h1>
-        <Character />
-        <hr/>
-        <Quotes />
+        <Router>
+            <Switch>
+              <Route exact path="/">
+                <h1>Welcome to the Universe</h1>
+                <Link to='/Characters'>Characters</Link>
+                <Link to='/Quotes'>Quotes</Link>
+                <Link to='/Admin'>Admin</Link>
+              </Route>
+              <Route path="/Characters">
+                <Character />
+              </Route>
+              <Route path="/Quotes">
+                <Quotes />
+              </Route>
+            </Switch>
+        </Router>
       </div>
     )
   }
