aba3eaeb77db17f5a3f4c2c637778da2b3b9b0f4
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-10-05 16:40:56 -0700

    fetch GET tests passing

--- src/fetch-utils.test.js
+++ src/fetch-utils.test.js
@@ -1,4 +1,4 @@
-import { fetchQuotes } from "./fetch-utils.js";
+import { fetchCharacters, fetchQuotes } from "./fetch-utils.js";
 
 test('gets all quotes', async() => {
     const expectation = {
@@ -10,6 +10,23 @@ test('gets all quotes', async() => {
     };
 
     const response = await fetchQuotes();
+
+    expect(response[0]).toEqual(expectation);
+})
+
+test('gets all characters', async() => {
+    const expectation = {
+        age: expect.any(String),
+        id: expect.any(Number),
+        character_name: expect.any(String),
+        character_id: expect.any(Number),
+        gem_type: expect.any(String),
+        weapon: expect.any(String),
+        species: expect.any(String),
+        img: expect.any(String)
+    };
+
+    const response = await fetchCharacters();
 
     expect(response[0]).toEqual(expectation);
 })
