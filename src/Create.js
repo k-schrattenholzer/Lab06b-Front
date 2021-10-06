82a574d105f584b987c747e4dbff11a51bd70946
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-10-05 22:25:00 -0700

    added create page

--- src/Create.js
+++ src/Create.js
@@ -1,43 +1,57 @@
 import React, { Component } from 'react';
-import request from 'superagent';
 import './App.css'
 import { fetchCharactersList } from './fetch-utils.js';
 
 export default class Create extends Component {
     state = {
-            characters:[],
-        }
-    
-        componentDidMount = async () => {
-            const charactersList = await fetchCharactersList();
-
-            this.setState({characters: charactersList})
+        character_list:[],
+        quote: '',
+        character_select: 1
     }
+    
+    componentDidMount = async () => {
+        const charactersList = await fetchCharactersList();
+
+        this.setState({character_list: charactersList})
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        console.log(this.state)
+    }
+
     render() {
-        console.log(this.state.characters)
-        return (
+       
+        return ( 
             <div className="Create">
                 <div className="CreateQuote">
                     <h4>Add Your Fav SU Quote!</h4>
-                    <form>
-                        <label>
+                    <form onSubmit={this.handleSubmit}>
+                        <label onChange={(e) => this.setState({ quote: e.target.value })}>
                             Quote
                             <input />
                         </label>
-                        <label>
+                        <label onChange={(e) => this.setState({ character_select: e.target.value })}>
                             Character
                                 <select>
                                 {
-                                    this.state.characters.map(
+                                    this.state.character_list
+                                    .sort((a, b) => (a.character_name > b.character_name) ? 1 : -1)
+                                    .map(
                                         character =>
                                         <option
-                                        value='character.id'
-                                        key='character.id'>
+                                        key={character.id}
+                                        value={character.id}
+                                        >
                                             {character.character_name}
                                         </option>
-                                )}
+                                    )}
                                 </select>
                         </label>
+                        <button>
+                            Add
+                        </button>
                     </form>
                 </div>
             </div>
