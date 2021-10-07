480aec6cbeaec98336de4dc476d28653ee1ca0e0
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-10-06 19:38:28 -0700

    styling on characters page

--- src/Character.js
+++ src/Character.js
@@ -31,13 +31,17 @@ export default class Character extends Component {
                         <div 
                         className="CharEl"
                         key={`${character_id}`}>
-                            <span>{character_name}</span>
+                            <span className="CharacterName">{character_name}</span>
                             <img src={img} alt={character_name}/>
                             <div className="StatsEl">
-                                <span>species: {species}</span>
-                                <span>weapon of choice: {weapon}</span>
-                                <span>age: {age}</span>
-                                <span>gem type: {gem_type}</span>
+                                <span className="StatSpan">
+                                    <h3>species:</h3> {species}</span>
+                                <span className="StatSpan">
+                                    <h3>weapon of choice:</h3> {weapon}</span>
+                                <span className="StatSpan">
+                                    <h3>age:</h3> {age}</span>
+                                <span className="StatSpan">
+                                    <h3>gem type:</h3> {gem_type}</span>
                             </div>
                         </div>
                     </Link>
